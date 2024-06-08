eaca9e61a6661b68c39903ccb3add800c52c26fa
hynduf <chauhuynhnoc@gmail.com>
2024-06-08 18:07:04 +0700

    Add docker

--- modules/system.nix
+++ modules/system.nix
@@ -11,7 +11,7 @@ in {
     users.users.${username} = {
         isNormalUser = true;
         description = username;
-        extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+        extraGroups = [ "networkmanager" "wheel" "video" "audio" "docker" ];
     };
 
     nix.settings = {
@@ -231,24 +231,19 @@ in {
     in
         formatted;
 
-    # Enable sound with pipewire.
     sound.enable = true;
     hardware.pulseaudio.enable = true;
-    # security.rtkit.enable = true;
-    # services.pipewire = {
-    #     enable = true;
-    #     alsa.enable = true;
-    #     alsa.support32Bit = true;
-    #     pulse.enable = true;
-    #     # If you want to use JACK applications, uncomment this
-    #     #jack.enable = true;
-
-    #     # use the example session manager (no others are packaged yet so this is enabled by default,
-    #     # no need to redefine it in your config for now)
-    #     #media-session.enable = true;
-    # };
 
     programs.noisetorch = {
         enable = true;
+    };
+
+    # enable docker
+    virtualisation.docker.enable = true;
+
+    # use docker without Root access (Rootless docker)
+    virtualisation.docker.rootless = {
+        enable = true;
+        setSocketVariable = true;
     };
 }
