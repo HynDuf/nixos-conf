1b60cc9e688da32b2b9a07919c151e3db3ed2893
hynduf <chauhuynhnoc@gmail.com>
2024-06-13 17:06:21 +0700

    Update override highlight color nvim

--- home/programs/neovim/config/lua/chadrc.lua
+++ home/programs/neovim/config/lua/chadrc.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 M.ui = {
-	hl_override = require("highlights").override,
+	hl_override = {
+		Visual = { bg = "#43364a" },
+		CursorLine = { bg = "#252636", fg = "#cba6f7" },
+		CursorLineNr = { fg = "#7aa2f7" },
+	},
 	theme = "catppuccin",
 	transparency = true,
 	tabufline = {
--- home/programs/neovim/config/lua/highlights.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.override = {
-	Visual = { bg = "#43364a" },
-	CursorLine = { fg = "#7aa2f7" },
-	CursorLineNr = { fg = "#7aa2f7" },
-}
-
-return M
